fbc9bc9877c4afeba71a7449ab7c4e8e1d5b2116
Yuan Zhang <zhangyuan@users.noreply.github.com>
2015-12-29 22:24:29 +0800

    add environment specific resources direocty and application properties file

--- src/main/java/com/evcheung/apps/opal/controllers/HomeController.java
+++ src/main/java/com/evcheung/apps/opal/controllers/HomeController.java
@@ -1,13 +1,24 @@
 package com.evcheung.apps.opal.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ConfigurationProperties(locations = "classpath:application.properties")
 public class HomeController {
+    @Value("${environment}")
+    private String environment;
+
     @RequestMapping("/")
     public String index() {
         return "Hello Spring World!";
+    }
+
+    @RequestMapping("/info")
+    public String info() {
+        return environment;
     }
 
 }
