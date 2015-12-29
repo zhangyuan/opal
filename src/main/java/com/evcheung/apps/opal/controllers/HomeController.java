6c85dee28ff7b57c517c2e53cee27641dbbe60f5
Yuan Zhang <zhangyuan@users.noreply.github.com>
2015-12-29 21:16:05 +0800

    create spring web application

---
+++ src/main/java/com/evcheung/apps/opal/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.evcheung.apps.opal.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Spring World!";
+    }
+
+}
